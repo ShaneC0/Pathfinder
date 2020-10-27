b400f9fc8cbeafb9ac7d3e3ac1fbba053fa8d172
ShaneC0 <connors.shane@hotmail.com>
2020-10-26 20:09:41 -0400

    reinitialized to remove typescript

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Grid from "./components/Grid";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nodeList: [],
+      cols: 50,
+      rows: 50
+    };
+  }
+
+  componentDidMount() {
+    this.getNodes();
+  }
+
+  getNodes() {
+    const nodes = [];
+    for(let i = 0; i < this.state.cols; i++) {
+      for(let j = 0; j < this.state.rows; j++) {
+        nodes.push({
+          variant: 'default',
+          row: j,
+          col: i
+        })
+      }
+    }
+    this.setState({nodeList: nodes})
+  }
+
+  render() {
+    return (
+      <div>
+        <Grid nodes={this.state.nodeList} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Node.jsx
@@ -0,0 +1,13 @@
+import "../styles/node.css";
+
+const Node = (props) => {
+  return (
+    <div
+      className={`node 
+        ${props.variant === "start" ? "node-start" : ""} 
+        ${props.variant === "end" ? "node-end" : ""}`}
+    ></div>
+  );
+};
+
+export default Node;
